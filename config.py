d71dd72797e382831dbf2a9ad5ff32acb2271112
carpedm20 <carpedm20@gmail.com>
2015-02-08 12:59:05 +0900

    sentiment ui template

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ DEBUG = True
 import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))  
 # SQLite for this example
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'what.db')
 DATABASE_CONNECT_OPTIONS = {}
 
 THREADS_PER_PAGE = 2
