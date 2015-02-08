3a9e29633e0babeaac501c7c172e4a7d7def44e7
carpedm20 <carpedm20@gmail.com>
2015-02-08 11:39:10 +0900

    update Basic

--- README.md
+++ README.md
@@ -5,7 +5,6 @@ Answer for the question "What to buy?"
 This project is inspired by [productchart](http://www.productchart.com/laptops/).
 
 
-
 ## Author
 
 Taehoon Kim / [@carpedm20](http://carpedm20.github.io/about/)
--- app/controllers.py
+++ app/controllers.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, render_template, \
                   flash, g, session, redirect, url_for
 
 from app import db
-from app.mod_auth.models import *
+from app.models import *
 
 buy_print = Blueprint('auth', __name__, url_prefix='/carpedm20/buy')
 
